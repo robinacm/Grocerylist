ff856ae3ea2bdc30a148cb1f2e8862cfd3f09f6f
robinacm <robinshirvani@hotmail.com>
2019-10-30 19:57:23 +0100

    added onclick to move between activities

--- app/src/main/java/com/example/grocerylist/AllItemsActivity.java
+++ app/src/main/java/com/example/grocerylist/AllItemsActivity.java
@@ -1,16 +1,38 @@
 package com.example.grocerylist;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.Button;
 
 public class AllItemsActivity extends AppCompatActivity {
+
+    Button btnAddGroceries;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_all_items);
 
+        btnAddGroceries = (Button)findViewById(R.id.button_addNewItem);
 
+        btnAddGroceries.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                allGroceriesActivity();
+            }
+        });
+
+        btnAddGroceries.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                allGroceriesActivity();
+            }
+        });
+    }
+
+    private void allGroceriesActivity() {
+        startActivity(new Intent(AllItemsActivity.this, MainActivity.class));
     }
 }
